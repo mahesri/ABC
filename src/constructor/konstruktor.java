6969f956c7b8f8aa3f2fcd6bf70556658643d326
mahesri <adi504097@gmail.com>
2024-02-19 22:38:43 +0700

    add : Package construktor

---
+++ src/constructor/konstruktor.java
@@ -0,0 +1,13 @@
+package constructor;
+
+public class konstruktor {
+
+    private String username;
+    private String password;
+
+    
+    public konstruktor() { // Ini merupakan konstruktor
+        System.out.println("Eksekusi method construktor...");
+    }
+
+}
