f04745cc72467ba25c49a2049a2e568462c42b01
mahesri <adi504097@gmail.com>
2024-02-28 19:18:00 +0700

    Add : tambahBelakang method

--- src/realL/praktik1.java
+++ src/realL/praktik1.java
@@ -23,9 +23,6 @@ public class praktik1 {
      akhir = null;   
     }
 
-    /**
-     * PAAN DAH!
-     */
     public static void tambahDepan(){
         String NAMA;
         String ALAMAT;
@@ -34,7 +31,7 @@ public class praktik1 {
         String HOBI[] = new String[3];
         float IPK;
         int bacaTombol = 0;
-
+        System.out.println("TAMBAH DEPAN : ");
         System.out.print("Silahkan masukan nama Anda : ");
         NAMA = in.next();
         System.out.print("Silahkan masukan alamat Anda : ");
@@ -78,6 +75,59 @@ public class praktik1 {
         }
     }
 
+    public static void tambahBelakang(){
+        String NAMA;
+        String ALAMAT;
+        int UMUR;
+        char JEKEL;
+        String HOBI[] = new String[3];
+        float IPK;
+        int bacaTombol = 0;
+
+        System.out.println("TAMBAH BELAKANG : ");
+        System.out.print("Silahkan masukan nama Anda : ");
+        NAMA = in.next();
+        System.out.print("Silahkan masukan alamat Anda : ");
+        ALAMAT = in.next();
+        System.out.print("Silahkan masukan umur Anda : ");
+        UMUR = in.nextInt();
+        System.out.print("Silahkan masukan jenis kelamin Anda : ");
+        try{
+            bacaTombol = System.in.read();
+        }catch(java.io.IOException e){}
+        JEKEL = (char)bacaTombol;
+
+        System.out.println("Silahkan masukan hobi Anda maks-3");
+        for(int i = 0; i <= 3-1;i++){
+            System.out.print("hobi ke-"+i+" : ");
+            HOBI[i] = in.next();
+        }
+
+        System.out.print("Silahkan masukan IPK Anda : ");
+        IPK = in.nextFloat();
+
+        simpul baru;
+        baru = new simpul();
+
+        baru.nama = NAMA;
+        baru.alamat = ALAMAT;
+        baru.umur = UMUR;
+        baru.jekel = JEKEL;
+        for(int i=0;i<=3-1;i++){
+            baru.hobi[i] = HOBI[i];
+        }
+        baru.ipk = IPK;
+
+        if (awal == null) {
+            awal = baru;
+            akhir = baru;
+            baru.kanan = null;
+        }else{
+            akhir = baru;
+            baru.kanan = awal;
+        }    
+    }
+
     public static void cetakSenarai(){
         if(awal == null){ //Jika senarai masih kosong
             System.out.println("Maaf senarai masih kosong!");
@@ -95,10 +145,10 @@ public class praktik1 {
                 System.out.print(bantu.alamat +"\t ");
                 System.out.print(bantu.umur +"\t ");
                 System.out.print(bantu.jekel +"\t ");
-                for(int i = 0;i<=3-1;i++){
-                    System.out.print(bantu.hobi[i] +"\t ");
-                }
-                System.out.print(bantu.ipk +"\t ");
+                System.out.print(bantu.hobi[0] +"\t ");
+                System.out.print(bantu.hobi[1] +"\t ");
+                System.out.print(bantu.hobi[2] +"\t ");
+                System.out.println(bantu.ipk +"\t ");
                 bantu = bantu.kanan;
             }
 
@@ -109,6 +159,10 @@ public class praktik1 {
         inisialisasiSenaraiKosong();
         tambahDepan();
         tambahDepan();
+        tambahDepan();
+        tambahBelakang();
+        tambahBelakang();
+        tambahBelakang();
         cetakSenarai();
     }
 }
