dc4d1ffd460d0929b836d8e26b43e38cab39da92
mahesri <adi504097@gmail.com>
2024-03-01 19:20:27 +0700

    change file from PercobaanLinkedList into prefik praktik

---
+++ src/collection/Praktik3.java
@@ -0,0 +1,39 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class Praktik3 {
+
+    LinkedList daftarMhs = new LinkedList();
+    
+    Praktik3(){  // A Constructor
+
+        daftarMhs.add("Livia");
+        daftarMhs.add("Intan");
+        daftarMhs.add("Mahes");
+        daftarMhs.add("Indri");
+        daftarMhs.add("Lastri");
+        daftarMhs.add("Dian");
+        daftarMhs.add("Sinta");
+        daftarMhs.add("Warjo");
+        daftarMhs.add("Laras");
+        System.out.println(daftarMhs.get(0));
+        System.out.println(daftarMhs.get(1));
+        System.out.println(daftarMhs.get(2));
+        System.out.println(daftarMhs.get(3));
+        System.out.println(daftarMhs.get(4));
+        System.out.println(daftarMhs.get(5));
+        System.out.println(daftarMhs.get(5));
+        System.out.println(daftarMhs.get(6));
+        System.out.println(daftarMhs.get(7));
+        System.out.println(daftarMhs.get(8));
+
+    }
+
+    public static void main(String args[]){
+
+    new Praktik3();     
+
+    }
+
+}
