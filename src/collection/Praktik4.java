da1ebce55eff8351407bb4dfadb7f342a0c9712d
mahesri <adi504097@gmail.com>
2024-03-01 21:31:12 +0700

    add :Praktik4 module

---
+++ src/collection/Praktik4.java
@@ -0,0 +1,36 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class Praktik4 {
+
+    LinkedList daftarMhs = new LinkedList();
+    
+    Praktik4(){  // A Constructor
+
+        daftarMhs.add("Livia");
+        daftarMhs.add("Intan");
+        daftarMhs.add("Mahes");
+        daftarMhs.add("Indri");
+        daftarMhs.add("Lastri");
+        daftarMhs.add("Dian");
+        daftarMhs.add("Sinta");
+        daftarMhs.add("Warjo");
+        daftarMhs.add("Laras");
+       
+        System.out.println("N = " + daftarMhs.size()); // 'size()' adalah method untuk mengetahui index elemen dari Linkedlist
+
+        for(int i = 0; i <= daftarMhs.size()-1;i++){
+            System.out.printf(i+1+" ");
+            System.out.print(daftarMhs.get(i)+"\n");
+        }
+        System.out.println(" ");
+    }
+
+    public static void main(String args[]){
+
+    new Praktik4();     
+
+    }
+
+}
