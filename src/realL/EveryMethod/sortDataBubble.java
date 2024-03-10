3baedff27181ba872e7f6e44a0e9d75eb6047258
mahesri <adi504097@gmail.com>
2024-03-10 23:35:37 +0700

    edit module

--- src/realL/EveryMethod/sortDataBubble.java
+++ src/realL/EveryMethod/sortDataBubble.java
@@ -1,4 +1,5 @@
 package realL.EveryMethod;
+// Fungsi pengurutan isi elemen heap / elemen simpul
 
 public class sortDataBubble {
 
@@ -10,19 +11,29 @@ public static void tukarNilai(simpul x, simpul y){
 	sementara.alamat = x.alamat;
 	sementara.umur = x.umur;
 	sementara.jekel = x.jekel;
+	sementara.hobi[0] = x.hobi[0];
+	sementara.hobi[1] = x.hobi[1];
+	sementara.hobi[2] = x.hobi[2];
 	sementara.ipk = x.ipk;
 
 	x.nama = y.nama;
 	x.alamat = y.alamat;
 	x.umur = y.umur;
 	x.jekel = y.jekel;
+	x.hobi[0] = y.hobi[0];
+	x.hobi[1] = y.hobi[1];
+	x.hobi[2] = y.hobi[2];
 	x.ipk = y.ipk;
 
 	y.nama = sementara.nama;
 	y.alamat = sementara.alamat;
 	y.umur = sementara.umur;
 	y.jekel = sementara.jekel;
+	y.hobi[0] = sementara.hobi[0];
+	y.hobi[1] = sementara.hobi[1];
+	y.hobi[2] = sementara.hobi[2];
 	y.ipk = sementara.ipk;
+
 	}
 
 //-------------------------------------------------
@@ -31,9 +42,30 @@ public static void tukarNilai(simpul x, simpul y){
 
 public static void mengurutkanDataBubble_TeknikTukarNilai(){
 
+int N = hitungJumlahSimpul();
+simpul A = null;
+simpul B = null;
+simpul berhenti = akhir.kanan;
 
+System.out.println("Banyaknya simpul = "+ hitungJumlahSimpul());
+for(int i){
+
+	A = awal;
+	B = awal.kanan;
+	int nomor = 1;
+while(B != berhenti){
+	if(A.nama.compareTo(B.nama)>0){
+		// Tukarkan elemen dari simpul A dan elemen dari simpul B
+		tukarNilai(A,B);
+	}
+	A = A.kanan;
+	B = B.kanan;
+	nomor++;
+}
+berhenti = A;
+}
+System.out.println("===PROSES PENGURUTAN BUBBLE SELESAI======");
 
 }
-
 
 }
