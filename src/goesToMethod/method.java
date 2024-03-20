5701ea396d34c9dea4034a65c339744cf427aa3b
mahesri <adi504097@gmail.com>
2024-03-20 19:25:07 +0700

    add methodku

---
+++ src/goesToMethod/method.java
@@ -0,0 +1,14 @@
+package goesToMethod;
+
+public class method {
+
+    // membuat method bernama mymethod
+   static void myMethod(){
+        System.out.println("Ini methodku!");
+    }
+
+    public static void main(String[]args){
+
+        // pemanggilan method
+        myMethod();
+    }}
