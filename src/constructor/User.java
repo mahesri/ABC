f1b4aae343c21c1d5eb7d97c1f02c759a32503a1
mahesri <adi504097@gmail.com>
2024-03-25 19:21:27 +0700

    add new user

--- src/constructor/User.java
+++ src/constructor/User.java
@@ -4,43 +4,48 @@ package constructor;
 
 public class User {
 
-    public String username; // diubah menjadi public agar String 
-    public String password;
+    String username; // diubah menjadi public agar String
+    String password;
 
             /* Catatan :
              * Cara membuat sebuah konstruktor adalah dengan menuliskan nama method constructor
              * sama seperti nama kelas.
-             * 
-             * Pastikan memberikan modifier public kepada Constructor, karena ia akan 
+             *
+             * Pastikan memberikan modifier public kepada Constructor, karena ia akan
              * dieksekusi saat pembuatan objek (instance).
              */
 
    public User (String username, String password){ // 'public User' merupakan konstruktornya || 'String username, String password' adalah 2 parameter yang diterima oleh konstruktor
 
-        this.username = username; // Memasukan parameter username dan password yang kemudian diset kedalam anggota `username` dan 'password' menggunakan kata kunci 'this' ke dalam instance baru berdasarkan variableyang berada di objek.
+        this.username = username; // Memasukan parameter username dan password yang kemudian diset kedalam anggota `username` dan 'password' menggunakan kata kunci 'this' ke dalam instance baru berdasarkan variable yang berada di objek.
         this.password = password;
     }
 
     }
 
 class DemoConstructor{
+
     public static void main(String[]args){
 
-     User MacbookAir = new User("Livia", "Mindfullness");
-     System.out.println("Username : " + MacbookAir.username);
-     System.out.println("Password : " + MacbookAir.password);
+     User user1 = new User("User1", "111");
+     System.out.println("Username : " + user1.username);
+     System.out.println("Password : " + user1.password);
+
+     User user2 = new User("user2", "222");
+     System.out.println("Nama saya "+user2.username);
+     System.out.println("Password saya "+user2.password);
 
      }
 }
 
 
 /* Catatan :
- * Construktor biasanya digunakan untuk inisialisasi(menyiapkan) data untuk class 
+ * Construktor biasanya digunakan untuk inisialisasi(menyiapkan) data untuk class
  * Untuk melakukan ini, kita harus membuat parameter inputan untuk konstruktor.
- * Contoh ada di baris 18 - 22. 
- * 
+ * Contoh ada di baris 18 - 22.
+ *
  * Mahesta Cerdas
- * 
+ *
  * Tambahan Catatan dari chat gpt :
  * Ketika Anda membuat objek MacbookAir menggunakan konstruktor User, konstruktor tersebut akan dieksekusi. Pada bagian this.username = username; dan this.password = password;, this merujuk kepada variabel anggota (instance variables) dari objek yang sedang dibuat.
 
